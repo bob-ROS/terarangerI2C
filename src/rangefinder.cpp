d26e126b96a837e2ca141da80f3f0a34095953b6
bob-ROS <fredrik.ano@gmail.com>
2018-07-16 14:30:39 +0200

    changed from std_msgs to sensor_msgs

--- src/rangefinder.cpp
+++ src/rangefinder.cpp
@@ -1,6 +1,9 @@
 #include <ros/ros.h>
-#include "std_msgs"
-#include "std_msgs/Range"
+//#include <std_msgs/Header.h>
+//#include "std_msgs"
+//#include "std_msgs"
+//#include "std_msgs/Range.h"
+#include <sensor_msgs/Range.h>
 
 #define address 0x31
 #define frequency 400
@@ -18,15 +21,17 @@ int main(int argc, char **argv){
 	ros::init(argc, argv, "Rangefinder");
 	ros::NodeHandle n;
 
-	std_msgs::Range my_sensor;
+	sensor_msgs::Range my_sensor;
 	while (ros::ok()){
+	/*
 	my_sensor.radiation_type = 1;
 	my_sensor.field_of_view = 0.034906585;
 	my_sensor.min_range = 0.5;
 	my_sensor.max_range = 60;
 	my_sensor.range |= 0x63;
+
 	// *(outbuffer - 1) |= 0x80;
 	ROS_INFO("Fistance: %f", my_sensor.range);
-
+	*/
 	}
 }
